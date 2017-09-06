256b68c2200b079069a17c2ebfe40c4d6385cd20
rlabrador <rlabrador2000@gmail.com>
2017-09-06 13:46:21 +0200

    ok port 8080

--- gulp/server.js
+++ gulp/server.js
@@ -38,6 +38,7 @@ function browserSyncInit(baseDir, browser) {
   browserSync.instance = browserSync.init({
     startPath: '/',
     server: server,
+    port: 8080,
     browser: browser,
     ghostMode: false
   });
